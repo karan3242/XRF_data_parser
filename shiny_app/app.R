c570c3c70fdcb2cd39327b564a48b5395be0e890
Karan Desai <workforce.karan@gmail.com>
2024-04-16 23:07:22 +0300

    Added a New Normals Tab to App. The High SD tab now useses Normalized data. Added a New Min Max range table to the Normal and High SD data tabs.

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -8,25 +8,29 @@
 #    https://shiny.posit.co/
 #
 
-
 library(shiny)
 library(readr)
 library(tidyverse)
 
 
-
 ui <- fluidPage(navbarPage(
   "XRF data Parsing",
+  tabPanel("Data table",
+           
+           mainPanel(
+             fileInput(
+               "file1",
+               "Choose CSV File",
+               accept = c("text/csv",
+                          "text/comma-separated-values,text/plain",
+                          ".csv")
+             ),
+             tags$h3("Main data"),
+             tableOutput("data_set")
+           )),
   tabPanel(
     "Data Overview",
-    sidebarPanel(
-      fileInput(
-        "file1",
-        "Choose CSV File",
-        accept = c("text/csv",
-                   "text/comma-separated-values,text/plain",
-                   ".csv")
-      ),
+    mainPanel(
       checkboxGroupInput(
         "elements",
         "Select Elements",
@@ -66,10 +70,10 @@ ui <- fluidPage(navbarPage(
         ) ,
         inline = TRUE,
         selected = c(
-          "Al",
-          "Si",
-          "S",
-          "Mn",
+          #"Al",
+          #"Si",
+          #"S",
+          #"Mn",
           "Fe",
           "Co",
           "Ni",
@@ -83,19 +87,24 @@ ui <- fluidPage(navbarPage(
           "Pb",
           "Bi"
         )
-      )
-      
-    ),
-    mainPanel(
-      tags$h3("Main data"),
+      ),
+      tags$h3("Selected Elements"),
       tableOutput("data_clean"),
+      
       tags$h3("Summary"),
       tableOutput("data_overview")
-      
-      
     )
   ),
-  
+  tabPanel(
+    "Normalized Data",
+    mainPanel(
+      tags$h3("Normalized Data"),
+      tableOutput("data_normal"),
+      tags$h3("Summary"),
+      tableOutput("data_overview_norm"),
+      tableOutput("data_overview_minmax")
+    )
+  ),
   tabPanel(
     "Data with High Standard Diviation",
     sidebarPanel(
@@ -113,12 +122,15 @@ ui <- fluidPage(navbarPage(
         min = 0,
         max = 3,
         value = 1
-        )
+      )
       
     ),
     mainPanel(
       tags$h3("High SD items - Summary"),
+      tags$h4("Mean and Standard Deviation"),
       tableOutput("high_sd_overview"),
+      tags$h4("Range"),
+      tableOutput("data_minmax_highsd"),
       tags$h3("High SD items - Data"),
       tableOutput("high_sd_data")
     )
@@ -134,13 +146,10 @@ server <- function(input, output) {
   data_set <- reactive({
     inFile <- input$file1
     
-    if (is.null(inFile))
-      return(NULL)
-    
     read_csv(inFile$datapath) %>%
       select(id = Lab_ID, everything()) %>%
       select(!contains("Error")) %>%
-      select(!`Collimation Status`) %>% 
+      select(!`Collimation Status`) %>%
       arrange(id)
     
     
@@ -149,21 +158,53 @@ server <- function(input, output) {
   # Clean data
   
   data_set_clean <- reactive({
-    
     data_set() %>%
-      select(id , starts_with(input$elements)) %>% 
+      select(id , starts_with(input$elements)) %>%
       select(id, sort(names(.)))
-    
-    
-    
   })
-
   
+  # Normal data
+  
+  data_set_normal <- reactive({
+    normalize_rows <- function(data) {
+      # Extracting IDs (assuming they are in the first column)
+      ids <- data[, 1]
+      
+      # Normalizing each row to 100%
+      normalized_data <-
+        t(apply(data[,-1, drop = FALSE], 1, function(row) {
+          row_sum <- sum(row, na.rm = TRUE)
+          
+          normalized_row <- row / row_sum * 100
+          
+          return(normalized_row)
+        }))
+      
+      # Combining IDs with normalized data
+      normalized_data <- cbind(ids, normalized_data)
+      
+      return(normalized_data)
+    }
+    
+    normalize_rows(data_set_clean())
+  })
   
   data_overview <- reactive({
     data_set_clean() %>%
       group_by(id) %>%
       summarise(across(everything(), list(mean = mean, sd = sd)))
+  })
+  
+  data_overview_norm <- reactive({
+    data_set_normal() %>%
+      group_by(id) %>%
+      summarise(across(everything(), list(mean = mean, sd = sd)))
+  })
+  
+  data_overview_minmax <-reactive({
+    data_set_normal() %>%
+      group_by(id) %>%
+      summarise(across(everything(), list(min = min, max = max)))
   })
   
   # Isolating High SD data
@@ -174,10 +215,10 @@ server <- function(input, output) {
         x >= input$cutoff
       }
     
-    data_overview() %>%
+    data_overview_norm() %>%
       filter(if_any(ends_with("sd"), high_sd)) %>%
       select(id, ends_with("sd")) %>%
-      select_if( ~ any(high_sd(.)))
+      select_if(~ any(high_sd(.)))
   })
   
   high_sd_overview <- reactive({
@@ -186,7 +227,20 @@ server <- function(input, output) {
              start = 1,
              stop = 2)
     
-    data_overview() %>%
+    data_overview_norm() %>%
+      filter(id %in% (high_sd()$id %>% as.array())) %>%
+      arrange(id) %>%
+      group_by(id) %>%
+      select(id, starts_with(columns))
+  })
+  
+  data_minmax_highsd <- reactive({
+    columns <-
+      substr(colnames(high_sd()[2:(max(col(high_sd())))]),
+             start = 1,
+             stop = 2)
+    
+    data_overview_minmax() %>%
       filter(id %in% (high_sd()$id %>% as.array())) %>%
       arrange(id) %>%
       group_by(id) %>%
@@ -199,29 +253,43 @@ server <- function(input, output) {
              start = 1,
              stop = 2)
     
+    
     outliers <- function(x) {
-      x > input$z_score | x < (input$z_score*(-1))
+      x > input$z_score | x < (input$z_score * (-1))
     }
     
-    data_set_clean() %>%
+    data_set_normal() %>%
       filter(id %in% (high_sd()$id %>% as.array())) %>%
       arrange(id) %>%
       group_by(id) %>%
-      select(id, starts_with(columns)) %>% 
+      select(id, starts_with(columns)) %>%
       mutate(across(everything(), list(z_score = scale))) %>%
-      select(id, sort(names(.))) %>% 
+      select(id, sort(names(.))) %>%
       filter(if_any(ends_with("z_score"), outliers))
     
   })
   
   # Outputs
   
+  output$data_set <- renderTable({
+    data_set()
+  })
+  
   output$data_clean <- renderTable({
     data_set_clean()
   })
   
+  output$data_normal <- renderTable({
+    data_set_normal()
+  })
+  
   output$data_overview <- renderTable({
     data_overview()
+    
+  })
+  
+  output$data_overview_norm <- renderTable({
+    data_overview_norm()
     
   })
   
@@ -236,7 +304,13 @@ server <- function(input, output) {
     high_sd_overview()
   })
   
+  output$data_overview_minmax <- renderTable({
+    data_overview_minmax()
+  })
+  output$data_minmax_highsd <- renderTable({
+    data_minmax_highsd()
+  })
+  
 }
 
 shinyApp(ui, server)
-
