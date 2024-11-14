5d8c25d464316b942de522cfbf1542447a869737
Karan Desai <workforce.karan@gmail.com>
2024-11-14 17:01:53 +0200

    Added features to save image and controle Y axis to Beam spectra plotter

--- beam_spectra_plotter/app.R
+++ beam_spectra_plotter/app.R
@@ -13,12 +13,13 @@ library(tidyverse)
 library(viridis)
 library(shinythemes)
 library(hrbrthemes)
+library(paletteer)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Ploting Data"),
+    titlePanel("XRF Beamspectra Plotter"),
   
         # Show a plot of the generated distribution
         sidebarPanel(
@@ -30,7 +31,26 @@ ui <- fluidPage(
                        ".csv")
                     ),
           uiOutput("readings"),
-          sliderInput("xaxis", "X-axis", 0, 41, value = c(0,41))),
+          sliderInput("xaxis", "X-axis", 0, 41, value = c(0,10)),
+          uiOutput("yaxis"),
+          textInput("title", "Plot Title", "Item", placeholder = "Item"),
+          tags$div(
+            style = "display: flex; flex-direction: column;",
+            tags$div(
+              style = "display: flex; flex-direction: row;",
+              textInput("dpi", "Select dpi:", 300, placeholder = "300"),
+              textInput("pwidth", "Select Width:", 1920, placeholder = "1920"),
+              textInput("pheight", "Select Height:", 1080, placeholder = "1080")
+            ),
+            tags$div(
+              style = "display: flex; flex-direction: row;",
+              textInput("dstpath", "Select Destination Folder:", "~/Pictures", placeholder = "~/Pictures"),
+              textInput("dstplot", "Select File name:", "plot", placeholder = "plot"),
+              actionButton("savePlot", "Save Plot")
+            )
+          )
+          
+          ),
         mainPanel(plotOutput("plot"))
     
 )
@@ -69,22 +89,39 @@ df3 <- reactive({
 })
 #output$df3 <-renderTable({df3()})
 
+output$yaxis <- renderUI({
+  lim <- max(max(df3()[,1]),max(df3()[,2]))
+  sliderInput("yaxis", "Y-axis", 0, lim, value = lim)
+})
+
+
 plot1 <- reactive({ggplot(df3(), aes(x=kev)) +
-  geom_line(aes(y = `1`, col = 'white'))+
-  geom_area(aes(y = `1`, fill = 'red'))+
-  geom_line(aes(y=`2`, col = 'white')) +
-  geom_area(aes(y = `2`, fill = 'blue'))+
-  xlim(input$xaxis[1], input$xaxis[2]) +
-  labs(y = "Counts", x = "Kev") +
-  theme_ipsum() +
-  theme(legend.position = "bottom", legend.title = element_blank())
+  geom_area(aes(y=`2`, fill ='Exposer 2')) +
+  geom_area(aes(y = `1`, fill = 'Exposer 1'))+
+  ylim = c(0, input$yaxis)) +
+  labs(y = "Counts/s", x = "Kev") +
+  theme_ipsum_ps() +
+  theme(legend.position = "bottom", legend.title = element_blank()) +
+  labs(title = input$title, col = "Energy") 
 })
 output$plot <- renderPlot({plot1()
    
   # plot2 <- plot1 + 
   #   geom_area(aes(y=`3`, fill = 'Exposer 3'))
 
-}, height = 700)
+})
+
+#Save plot to file
+observeEvent(input$savePlot, {
+  ggsave(
+    filename = file.path(input$dstpath, paste0(input$dstplot,"_", input$title, ".png")),
+    plot = plot1(),
+    width = as.integer(input$pwidth),
+    height = as.integer(input$pheight),
+    units = "px",
+    dpi = as.integer(input$dpi)
+  )
+})
 
 
 } # Run the application 
