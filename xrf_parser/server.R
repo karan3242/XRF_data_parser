177fcee269fb30078fa2e0e25854a753ba1ad9ed
Karan Desai <workforce.karan@gmail.com>
2024-11-16 14:23:56 +0200

    Improved the element selection process by showing only elements which are scaned by the XRF.

--- xrf_parser/server.R
+++ xrf_parser/server.R
@@ -14,75 +14,52 @@ library(viridis)
 library(plotly)
 library(writexl)
 
-el = c(
-       "Ac", "Ag", "Al", "As", "At", "Au", "Ba", "Bh", "Bi", "Br", "Ca", "Cd", "Ce", "Cl", "Cn", "Co", "Cr", "Cs", "Cu", "Db", "Ds", "Dy", "Er", "Eu", "Fe", "Fl", "Fr", "Ga", "Gd", "Ge", "Hf", "Hg", "Ho", "Hs", "I", "In", "Ir", "K", "La", "Lu", "Lv", "Mc", "Mg", "Mn", "Mo", "Mt", "Nb", "Nd", "Nh", "Ni", "Np", "Og", "Os", "P", "Pa", "Pb", "Pd", "Pm", "Po", "Pr", "Pt", "Pu", "Ra", "Rb", "Re", "Rf", "Rg", "Rh", "Ru", "S", "Sb", "Sc", "Se", "Sg", "Si", "Sm", "Sn", "Sr", "Ta", "Tb", "Tc", "Te", "Th", "Ti", "Tl", "Tm", "Ts", "U", "V", "W", "Y", "Yb", "Zn", "Zr"
-)
-
 function(input, output, session) {
-
   #Select all check box
-
-  observe({
-    updateCheckboxGroupInput(
-      session,
-      "elements",
-      inline = TRUE,
-      choices = el,
-      selected = if(input$all) el
-    )
-  })
-
+  
+  
+  
   # Base data set with pre-processing
-
+  
   data_set <- reactive({
     inFile <- input$file1
-
+    
     df <- read_csv(inFile$datapath) %>%
       select(id = Lab_ID, everything()) %>%
       arrange(id)
-
+    
     df[df == '<LOD'] <- NA
-
+    
     df <- df %>%
       mutate_at(
         vars(
-          -id,
-          -Date,
-          -Time,
-          -Units,
-          -Description,
-          -matches("Test Label"),
-          -matches("Collimation Status"),
-          -matches("Method Name"),
-          -matches("Instrument Serial Num"),
-          -matches("Reading #")
+          -id,-Date,-Time,-Units,-Description,-matches("Test Label"),-matches("Collimation Status"),-matches("Method Name"),-matches("Instrument Serial Num"),-matches("Reading #")
         ),
         as.numeric
       )
-
+    
     df[is.na(df)] <- 0
-
+    
     df$id <- as.character(df$id)
     df$`Instrument Serial Num` <-
       as.integer(df$`Instrument Serial Num`)
     df$`Reading #` <- as.integer(df$`Reading #`)
     df$`Test Label` <- as.integer(df$`Test Label`)
-
+    
     df$Notes[df$Notes == 0] <- NA
-
+    
     df$Notes <- as.character(df$Notes)
-
+    
     return(df)
   })
-  output$data_set <- renderTable({data_set()})
-
-  # Clean data
-
+  output$data_set <- renderTable({
+    data_set()
+  })
+  
   # Lab items output selection
-
+  
   output$lab_items <- renderUI({
     req(data_set())
-    print(unique(data_set()$id))
     checkboxGroupInput(
       "lab_id",
       "Select Lab Items",
@@ -91,145 +68,175 @@ function(input, output, session) {
       selected = unique(data_set()$id)
     )
   })
-  output$data_summary_minmax <- renderTable({data_summary_minmax()})
-
+  output$data_summary_minmax <- renderTable({
+    data_summary_minmax()
+  })
+  
+  output$elm <- renderUI({
+    req(data_set())
+    elmt <- sort(gsub('.{14}$', '', colnames(select(
+      data_set(), contains("Concentration")
+    ))))
+    checkboxGroupInput(
+      "elm",
+      "Select Elements",
+      choices = elmt,
+      inline = TRUE,
+      selected = elmt
+    )
+  })
+  
+  #element elections
+  
   # Render data set after items and elements selected
-
+  
   data_set_clean <- reactive({
     req(input$lab_id, data_set())
     data_set() %>%
       select(!contains("Error")) %>%
       select(!`Collimation Status`) %>%
-      select(id , starts_with(input$elements)) %>%
+      select(id , starts_with(input$elm)) %>%
       select(id, contains("Concentration")) %>%
       select(id, sort(names(.))) %>%
       filter(id %in% input$lab_id)
   })
-  output$data_clean <- renderTable({data_set_clean()})
-
+  output$data_clean <- renderTable({
+    data_set_clean()
+  })
+  
   # Mean and SD of clean Data
-
+  
   data_overview <- reactive({
     data_set_clean() %>%
       group_by(id) %>%
       summarise(across(everything(), list(mean = mean, sd = sd)))
   })
-  output$data_overview <- renderTable({data_overview()})
-
+  output$data_overview <- renderTable({
+    data_overview()
+  })
+  
   # Min-Max of clean Data
-
-  data_summary_minmax <-reactive({
+  
+  data_summary_minmax <- reactive({
     data_set_clean() %>%
       group_by(id) %>%
       summarise(across(everything(), list(min = min, max = max)))
   })
-  output$data_summary_minmax <- renderTable({data_summary_minmax()})
-
+  output$data_summary_minmax <- renderTable({
+    data_summary_minmax()
+  })
+  
   # Normalized Data set
-
+  
   data_set_normal <- reactive({
     normalize_rows <- function(data) {
       # Extracting IDs (assuming they are in the first column)
       ids <- data[, 1]
-
+      
       # Normalizing each row to 100%
       normalized_data <-
-        t(apply(data[,-1, drop = FALSE], 1, function(row) {
+        t(apply(data[, -1, drop = FALSE], 1, function(row) {
           row_sum <- sum(row, na.rm = TRUE)
-
+          
           normalized_row <- row / row_sum * 100
-
+          
           return(normalized_row)
         }))
-
+      
       # Combining IDs with normalized data
       normalized_data <- cbind(ids, normalized_data)
-
+      
       return(normalized_data)
     }
-
+    
     normalize_rows(data_set_clean())
   })
-  output$data_normal <- renderTable({data_set_normal()})
-
+  output$data_normal <- renderTable({
+    data_set_normal()
+  })
+  
   # Mean and SD of Normalized data
-
+  
   data_overview_norm <- reactive({
     data_set_normal() %>%
       group_by(id) %>%
       summarise(across(everything(), list(mean = mean, sd = sd)))
   })
-  output$data_overview_norm <- renderTable({data_overview_norm()})
-
+  output$data_overview_norm <- renderTable({
+    data_overview_norm()
+  })
+  
   # Min-Max of Normalized data
-
-  data_overview_minmax <-reactive({
+  
+  data_overview_minmax <- reactive({
     data_set_normal() %>%
       group_by(id) %>%
       summarise(across(everything(), list(min = min, max = max)))
   })
-  output$data_overview_minmax <- renderTable({data_overview_minmax()})
-
+  output$data_overview_minmax <- renderTable({
+    data_overview_minmax()
+  })
+  
   # Isolating High SD data
-
+  
   high_sd <- reactive({
     high_sd <-
       function(x) {
         x >= input$cutoff
       }
-
+    
     data_overview_norm() %>%
       filter(if_any(ends_with("sd"), high_sd)) %>%
       select(id, ends_with("sd")) %>%
-      select_if(~ any(high_sd(.)))
+      select_if( ~ any(high_sd(.)))
   })
-  output$high_sd <- renderTable({high_sd()})
-
+  output$high_sd <- renderTable({
+    high_sd()
+  })
+  
   # Mean and SD of date with high SD determined by Threshold
-
+  
   high_sd_overview <- reactive({
     columns <-
-      substr(colnames(high_sd()[2:(max(col(high_sd())))]),
-             start = 1,
-             stop = 2)
-
+      substr(colnames(high_sd()[2:(max(col(high_sd())))]), start = 1, stop = 2)
+    
     data_overview_norm() %>%
       filter(id %in% (high_sd()$id %>% as.array())) %>%
       arrange(id) %>%
       group_by(id) %>%
       select(id, starts_with(columns))
   })
-  output$high_sd_overview <- renderTable({high_sd_overview()})
-
+  output$high_sd_overview <- renderTable({
+    high_sd_overview()
+  })
+  
   # Table for min-max range of items with high standard divination determined by Threshold
-
+  
   data_minmax_highsd <- reactive({
     columns <-
-      substr(colnames(high_sd()[2:(max(col(high_sd())))]),
-             start = 1,
-             stop = 2)
-
+      substr(colnames(high_sd()[2:(max(col(high_sd())))]), start = 1, stop = 2)
+    
     data_overview_minmax() %>%
       filter(id %in% (high_sd()$id %>% as.array())) %>%
       arrange(id) %>%
       group_by(id) %>%
       select(id, starts_with(columns))
   })
-  output$data_minmax_highsd <- renderTable({data_minmax_highsd()})
-
+  output$data_minmax_highsd <- renderTable({
+    data_minmax_highsd()
+  })
+  
   # Filters items with Z score above set threshold
-
+  
   high_sd_data <- reactive({
     columns <-
-      substr(colnames(high_sd()[2:(max(col(high_sd())))]),
-             start = 1,
-             stop = 2)
-
-
+      substr(colnames(high_sd()[2:(max(col(high_sd())))]), start = 1, stop = 2)
+    
+    
     outliers <- function(x) {
       x > input$z_score | x < (input$z_score * (-1))
     }
-
+    
     data_set_normal() %>%
       filter(id %in% (high_sd()$id %>% as.array())) %>%
       arrange(id) %>%
@@ -238,33 +245,39 @@ function(input, output, session) {
       mutate(across(everything(), list(z_score = scale))) %>%
       select(id, sort(names(.))) %>%
       filter(if_any(ends_with("z_score"), outliers))
-
+    
   })
-  output$high_sd_data <- renderTable({high_sd_data()})
-
+  output$high_sd_data <- renderTable({
+    high_sd_data()
+  })
+  
   # Piloting the data
-
+  
   data_plot <- reactive({
     data_mean <-
       data_overview_norm() %>%
-      pivot_longer(cols = (contains("mean")),
-                   names_to = "Type",
-                   values_to = "Value_mean") %>%
+      pivot_longer(
+        cols = (contains("mean")),
+        names_to = "Type",
+        values_to = "Value_mean"
+      ) %>%
       select(id, Type, Value_mean)
-
+    
     data_sd <-
       data_overview_norm() %>%
-      pivot_longer(cols = (contains("sd")),
-                   names_to = "Type",
-                   values_to = "Value_sd") %>%
+      pivot_longer(
+        cols = (contains("sd")),
+        names_to = "Type",
+        values_to = "Value_sd"
+      ) %>%
       select(id, Type, Value_sd)
-
+    
     data_mean$Type <- data_mean$Type %>% substr(1, 2)
     data_sd$Type <- data_sd$Type %>% substr(1, 2)
-
+    
     data_join <-
       full_join(data_mean, data_sd, by = join_by(id, Type))
-
+    
     ggplotly(
       ggplot(data_join) +
         geom_bar(
@@ -286,41 +299,38 @@ function(input, output, session) {
         ) +
         scale_fill_viridis(discrete = TRUE, name = "") +
         theme_classic() +
-        theme(axis.line = element_blank(),
-              axis.ticks = element_blank(),
-              )+
-        labs(x = NULL,
-             y = "Element %",
-             title = "Elemet values - Normalized Data")
+        theme(axis.line = element_blank(), axis.ticks = element_blank(), ) +
+        labs(x = NULL, y = "Element %", title = "Elemet values - Normalized Data")
     )
-
+    
   })
-  output$plot <- renderPlotly({data_plot()})
-
-
+  output$plot <- renderPlotly({
+    data_plot()
+  })
+  
+  
   output$dl <- downloadHandler(
-
     filename = function() {
       paste0("df_dmodel", "_Table", ".xlsx")
     },
-    content = function(file){
-      tbl_primary<- data_set()
-      tbl_selected_itmes<- data_set_clean()
-      tbl_elements_overview<- data_overview()
-      tbl_elements_minmax<- data_summary_minmax()
-      tbl_normalized<- data_set_normal()
-      tbl_normal_overview<- data_overview_norm()
-      tbl_normal_minmax<- data_overview_minmax()
-      tbl_highsd<- high_sd()
-      tbl_highsd_overview<- high_sd_overview()
-      tbl_highsd_minmax<- data_minmax_highsd()
-      tbl_highsd_values<- high_sd_data()
-
+    content = function(file) {
+      tbl_primary <- data_set()
+      tbl_selected_itmes <- data_set_clean()
+      tbl_elements_overview <- data_overview()
+      tbl_elements_minmax <- data_summary_minmax()
+      tbl_normalized <- data_set_normal()
+      tbl_normal_overview <- data_overview_norm()
+      tbl_normal_minmax <- data_overview_minmax()
+      tbl_highsd <- high_sd()
+      tbl_highsd_overview <- high_sd_overview()
+      tbl_highsd_minmax <- data_minmax_highsd()
+      tbl_highsd_values <- high_sd_data()
+      
       sheets <- mget(ls(pattern = "tbl")) # getting all objects in your environment with tbl in the name
       #names(sheets) <- paste0("sheet", seq_len(length(sheets))) # changing the names in your list
       writexl::write_xlsx(sheets, path = file) # saving the file
     }
   )
-
-
+  
+  
 }
--- xrf_parser/ui.R
+++ xrf_parser/ui.R
@@ -22,36 +22,27 @@ fluidPage(
     "XRF Data Parsing",
     
     # Primary Data tab
-    tabPanel("Primary Data",
-             fileInput(
-                 "file1",
-                 "Choose CSV File",
-                 accept = c("text/csv",
-                            "text/comma-separated-values,text/plain",
-                            ".csv")
-               ),
-               tagList(
-                 tags$h1("Primary Data"),
-                 tags$p("<LOD values have been converted to 0"),
-                 tableOutput("data_set")
-               )
-             ),
+    tabPanel(
+      "Primary Data",
+      fileInput(
+        "file1",
+        "Choose CSV File",
+        accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")
+      ),
+      tagList(
+        tags$h1("Primary Data"),
+        tags$p("<LOD values have been converted to 0"),
+        tableOutput("data_set")
+      )
+    ),
     
     # Data Overview tab
     tabPanel(
       "Data Overview",
-        uiOutput("lab_items"),
-        # checkboxInput("it_all", "Select all", value = TRUE),
-        checkboxGroupInput(
-          "elements",
-          "Select Elements",
-          choices = c( "Ag", "Al", "As", "Au", "Bi", "Cd", "Co", "Cr", "Cu", "Fe", "Hf", "Mg", "Mn", "Mo", "Nb", "Ni", "P", "Pb", "Pd", "Re", "S", "Sb", "Sc", "Si", "Sn", "Sr", "Ta", "Ti", "V", "W", "Zn", "Zr")
-        ),
-        checkboxInput("all", "Select all", value = TRUE),
+      uiOutput("lab_items"),
+      uiOutput("elm"),
       navset_tab(
-        nav_panel("Selected Items",
-                  
-                  tableOutput("data_clean")),
+        nav_panel("Selected Items", tableOutput("data_clean")),
         nav_panel(
           "Elements Overview",
           tableOutput("data_overview"),
@@ -61,59 +52,57 @@ fluidPage(
     ),
     
     # Normalized Data tab
-    tabPanel("Normalized Data",
-             navset_tab(
-               nav_panel("Normalized Data", tableOutput("data_normal")),
-               nav_panel(
-                 "Summary",
-                 tableOutput("data_overview_norm"),
-                 tableOutput("data_overview_minmax")
-               )
-             )),
+    tabPanel("Normalized Data", navset_tab(
+      nav_panel("Normalized Data", tableOutput("data_normal")),
+      nav_panel(
+        "Summary",
+        tableOutput("data_overview_norm"),
+        tableOutput("data_overview_minmax")
+      )
+    )),
     
     # High SD Readings tab
-    tabPanel("Outliers",
-             navset_tab(
-               nav_panel(
-                 "Summary",
-                 sliderInput(
-                   "cutoff",
-                   "Deviation Percentage Cutoff",
-                   min = 0,
-                   max = 10,
-                   value = 3,
-                   step = 0.01,
-                   ticks = FALSE
-                 ),
-                 tags$h2("Mean and Standard Deviation"),
-                 tableOutput("high_sd_overview"),
-                 tags$h2("Range"),
-                 tableOutput("data_minmax_highsd")
-               ),
-               nav_panel(
-                 "Readings",
-                 sliderInput(
-                   "z_score",
-                   "Deviation Steps",
-                   min = 0,
-                   max = 3,
-                   value = 1,
-                   step = 0.01,
-                   ticks = FALSE
-                 ),
-                 tableOutput("high_sd_data")
-               )
-             )),
+    tabPanel("Outliers", navset_tab(
+      nav_panel(
+        "Summary",
+        sliderInput(
+          "cutoff",
+          "Deviation Percentage Cutoff",
+          min = 0,
+          max = 10,
+          value = 3,
+          step = 0.01,
+          ticks = FALSE
+        ),
+        tags$h2("Mean and Standard Deviation"),
+        tableOutput("high_sd_overview"),
+        tags$h2("Range"),
+        tableOutput("data_minmax_highsd")
+      ),
+      nav_panel(
+        "Readings",
+        sliderInput(
+          "z_score",
+          "Deviation Steps",
+          min = 0,
+          max = 3,
+          value = 1,
+          step = 0.01,
+          ticks = FALSE
+        ),
+        tableOutput("high_sd_data")
+      )
+    )),
     
     # Plot tab
-    tabPanel("Plot",
-            # tags$h1("Plot"),
-            plotlyOutput("plot"),
-            downloadButton('dl',"Download the data"),
-            ),
+    tabPanel(
+      "Plot",
+      # tags$h1("Plot"),
+      plotlyOutput("plot"),
+      downloadButton('dl', "Download the data"),
+    ),
     
     # About Page
-    tabPanel("About",
-             includeMarkdown("../README.md"))
+    tabPanel("About", includeMarkdown("../README.md"))
   )
 )
